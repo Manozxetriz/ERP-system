ef867dd39aa3d4ce633f980716037f1a75763c7c
Manozxetriz <xetrizmanoz3@gmail.com>
2026-08-01 12:24:02 +0545

    dbsetup

---
+++ ManufacturingERP.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManufacturingERP.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ ManufacturingERP.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using ManufacturingERP.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManufacturingERP.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
